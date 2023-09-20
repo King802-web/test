a1d704e1892e69753adb83e692e11109439303c2
unknown <wanghongjiang@neura-robotics.cn>
2023-09-20 10:38:06 +0800

    音频播放

--- ffmpeg-test/main.cpp
+++ ffmpeg-test/main.cpp
@@ -1,9 +1,6 @@
-#include "logger.h"
-#include <iostream>
-
 /**
- * 最简单的SDL2播放音频的例子（SDL2播放PCM）
- * Simplest Audio Play SDL2 (SDL2 play PCM)
+ * 最简单的基于FFmpeg的音频播放器 2
+ * Simplest FFmpeg Audio Player 2
  *
  * 雷霄骅 Lei Xiaohua
  * leixiaohua1020@126.com
@@ -11,124 +8,288 @@
  * Communication University of China / Digital TV Technology
  * http://blog.csdn.net/leixiaohua1020
  *
- * 本程序使用SDL2播放PCM音频采样数据。SDL实际上是对底层绘图
- * API（Direct3D，OpenGL）的封装，使用起来明显简单于直接调用底层
- * API。
+ * 本程序实现了音频的解码和播放。
+ * 是最简单的FFmpeg音频解码方面的教程。
+ * 通过学习本例子可以了解FFmpeg的解码流程。
  *
- * 函数调用步骤如下:
+ * 该版本使用SDL 2.0替换了第一个版本中的SDL 1.0。
+ * 注意：SDL 2.0中音频解码的API并无变化。唯一变化的地方在于
+ * 其回调函数的中的Audio Buffer并没有完全初始化，需要手动初始化。
+ * 本例子中即SDL_memset(stream, 0, len);
  *
- * [初始化]
- * SDL_Init(): 初始化SDL。
- * SDL_OpenAudio(): 根据参数（存储于SDL_AudioSpec）打开音频设备。
- * SDL_PauseAudio(): 播放音频数据。
+ * This software decode and play audio streams.
+ * Suitable for beginner of FFmpeg.
  *
- * [循环播放数据]
- * SDL_Delay(): 延时等待播放完成。
+ * This version use SDL 2.0 instead of SDL 1.2 in version 1
+ * Note:The good news for audio is that, with one exception,
+ * it's entirely backwards compatible with 1.2.
+ * That one really important exception: The audio callback
+ * does NOT start with a fully initialized buffer anymore.
+ * You must fully write to the buffer in all cases. In this
+ * example it is SDL_memset(stream, 0, len);
  *
- * This software plays PCM raw audio data using SDL2.
- * SDL is a wrapper of low-level API (DirectSound).
- * Use SDL is much easier than directly call these low-level API.
- *
- * The process is shown as follows:
- *
- * [Init]
- * SDL_Init(): Init SDL.
- * SDL_OpenAudio(): Opens the audio device with the desired
- *					parameters (In SDL_AudioSpec).
- * SDL_PauseAudio(): Play Audio.
- *
- * [Loop to play data]
- * SDL_Delay(): Wait for completetion of playback.
+ * Version 2.0
  */
-
 #include <stdio.h>
-#include <tchar.h>
+#include <stdlib.h>
+#include <string.h>
+#include "logger.h"
 
+#define __STDC_CONSTANT_MACROS
+
+#ifdef _WIN32
+//Windows
 extern "C"
 {
+#include "libavcodec/avcodec.h"
+#include "libavformat/avformat.h"
+#include "libswresample/swresample.h"
 #include "SDL2/SDL.h"
 };
+#else
+//Linux...
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libswresample/swresample.h>
+#include <SDL2/SDL.h>
+#ifdef __cplusplus
+};
+#endif
+#endif
+
+#define MAX_AUDIO_FRAME_SIZE 192000 // 1 second of 48khz 32bit audio
+
+//Output PCM
+#define OUTPUT_PCM 1
+//Use SDL
+#define USE_SDL 1
 
 //Buffer:
 //|-----------|-------------|
 //chunk-------pos---len-----|
-static  Uint8  *audio_chunk;
+static  Uint8   *audio_chunk;
 static  Uint32  audio_len;
-static  Uint8  *audio_pos;
+static  Uint8   *audio_pos;
 
-/* Audio Callback
- * The audio function callback takes the following parameters:
+/* The audio function callback takes the following parameters:
  * stream: A pointer to the audio buffer to be filled
  * len: The length (in bytes) of the audio buffer
- *
 */
 void  fill_audio(void *udata,Uint8 *stream,int len){
     //SDL 2.0
     SDL_memset(stream, 0, len);
-    if(audio_len==0)		/*  Only  play  if  we  have  data  left  */
-            return;
+    if(audio_len==0)
+        return;
+
     len=(len>audio_len?audio_len:len);	/*  Mix  as  much  data  as  possible  */
 
     SDL_MixAudio(stream,audio_pos,len,SDL_MIX_MAXVOLUME);
     audio_pos += len;
     audio_len -= len;
 }
+//-----------------
 
 #undef main
 int main(int argc, char* argv[])
 {
+    AVFormatContext	*pFormatCtx;
+    int				i, audioStream;
+    AVCodecContext	*pCodecCtx;
+    const AVCodec   *pCodec;
+    AVPacket		*packet;
+    uint8_t			*out_buffer;
+    AVFrame			*pFrame;
+    SDL_AudioSpec   wanted_spec;
+    int             ret;
+    uint32_t        len = 0;
+    int             got_picture;
+    int             index = 0;
+    int64_t         in_channel_layout;
+    struct SwrContext *au_convert_ctx;
+
+    FILE            *pFile=NULL;
+    //char url[]="xiaoqingge.mp3";
+
+    pFormatCtx = avformat_alloc_context();
+    //Open
+    if(avformat_open_input(&pFormatCtx,argv[1],NULL,NULL)!=0){
+        printf("Couldn't open input stream.\n");
+        return -1;
+    }
+    // Retrieve stream information
+    if(avformat_find_stream_info(pFormatCtx,NULL)<0){
+        printf("Couldn't find stream information.\n");
+        return -1;
+    }
+    // Dump valid information onto standard error
+    av_dump_format(pFormatCtx, 0, argv[1], false);
+
+    // Find the first audio stream
+    audioStream=-1;
+    for(i=0; i < pFormatCtx->nb_streams; i++)
+        if(pFormatCtx->streams[i]->codecpar->codec_type==AVMEDIA_TYPE_AUDIO)
+        {
+            audioStream=i;
+            break;
+        }
+
+    if(audioStream==-1)
+    {
+        printf("Didn't find a audio stream.\n");
+        return -1;
+    }
+
+    // Get a pointer to the codec context for the audio stream
+    pCodecCtx=avcodec_alloc_context3(NULL);
+    avcodec_parameters_to_context(pCodecCtx,pFormatCtx->streams[audioStream]->codecpar);
+    // Find the decoder for the audio stream
+    pCodec=avcodec_find_decoder(pCodecCtx->codec_id);
+    if(pCodec==NULL){
+        printf("Codec not found.\n");
+        return -1;
+    }
+
+    // Open codec
+    if(avcodec_open2(pCodecCtx, pCodec,NULL)<0){
+        printf("Could not open codec.\n");
+        return -1;
+    }
+
+
+#if OUTPUT_PCM
+    pFile=fopen("output.pcm", "wb");
+#endif
+
+    packet=(AVPacket *)av_malloc(sizeof(AVPacket));
+    av_init_packet(packet);
+
+    //Out Audio Param
+    uint64_t out_channel_layout=AV_CH_LAYOUT_STEREO;
+    //nb_samples: AAC-1024 MP3-1152
+    int out_nb_samples=pCodecCtx->frame_size;
+    AVSampleFormat out_sample_fmt=AV_SAMPLE_FMT_S16;
+    int out_sample_rate=pCodecCtx->sample_rate;
+    int out_channels=av_get_channel_layout_nb_channels(out_channel_layout);
+    //Out Buffer Size
+    int out_buffer_size=av_samples_get_buffer_size(NULL,out_channels ,out_nb_samples,out_sample_fmt, 1);
+
+    out_buffer=(uint8_t *)av_malloc(MAX_AUDIO_FRAME_SIZE*2);
+    pFrame=av_frame_alloc();
+//SDL------------------
+#if USE_SDL
     //Init
-    if(SDL_Init(SDL_INIT_AUDIO | SDL_INIT_TIMER)) {
+    if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER)) {
         printf( "Could not initialize SDL - %s\n", SDL_GetError());
         return -1;
     }
     //SDL_AudioSpec
-    SDL_AudioSpec wanted_spec;
-    wanted_spec.freq = 44100;
+    wanted_spec.freq = out_sample_rate;
     wanted_spec.format = AUDIO_S16SYS;
-    wanted_spec.channels = 2;
+    wanted_spec.channels = out_channels;
     wanted_spec.silence = 0;
-    wanted_spec.samples = 1024;
+    wanted_spec.samples = out_nb_samples;
     wanted_spec.callback = fill_audio;
+    wanted_spec.userdata = pCodecCtx;
 
     if (SDL_OpenAudio(&wanted_spec, NULL)<0){
         printf("can't open audio.\n");
         return -1;
     }
+#endif
 
-    //FILE *fp=fopen("NocturneNo2inEflat_44.1k_s16le.pcm","rb+");
-    FILE *fp=fopen(argv[1],"rb+");
-    if(fp==NULL){
-        printf("cannot open this file\n");
-        return -1;
-    }
+    //FIX:Some Codec's Context Information is missing
+    in_channel_layout=av_get_default_channel_layout(pCodecCtx->channels);
+    //Swr
 
-    int pcm_buffer_size=4096;
-    char *pcm_buffer=(char *)malloc(pcm_buffer_size);
-    int data_count=0;
+    au_convert_ctx = swr_alloc();
+    au_convert_ctx=swr_alloc_set_opts(au_convert_ctx,out_channel_layout, out_sample_fmt, out_sample_rate,
+        in_channel_layout,pCodecCtx->sample_fmt , pCodecCtx->sample_rate,0, NULL);
+    swr_init(au_convert_ctx);
 
     //Play
     SDL_PauseAudio(0);
 
-    while(1){
-        if (fread(pcm_buffer, 1, pcm_buffer_size, fp) != pcm_buffer_size){
-            // Loop
-            fseek(fp, 0, SEEK_SET);
-            fread(pcm_buffer, 1, pcm_buffer_size, fp);
-            data_count=0;
-        }
-        printf("Now Playing %10d Bytes data.\n",data_count);
-        data_count+=pcm_buffer_size;
-        //Set audio buffer (PCM data)
-        audio_chunk = (Uint8 *) pcm_buffer;
-        //Audio buffer length
-        audio_len =pcm_buffer_size;
-        audio_pos = audio_chunk;
+    while(av_read_frame(pFormatCtx, packet)>=0){
+        if(packet->stream_index==audioStream){
+            //ret = avcodec_decode_audio4( pCodecCtx, pFrame,&got_picture, packet);
+            ret = avcodec_send_packet(pCodecCtx,packet);
+            if(ret < 0)
+            {
+                LOG(ERROR,"avcodec_send_packet failed");
+                break;
+            }
+            while(ret >= 0)
+            {
+                ret = avcodec_receive_frame(pCodecCtx,pFrame);
+                if (ret == AVERROR(EAGAIN) )
+                {
+                    // 需要更多数据或已经解码完成，跳出内部循环
+                    LOG(WARN,"AVERROR(EAGAIN)");
+                    break;
+                } else if(ret == AVERROR_EOF)
+                {
+                    // 需要更多数据或已经解码完成，跳出内部循环
+                    LOG(WARN,"AVERROR_EOF");
+                    break;
+                }
+                else if (ret < 0) {
+                    LOG(ERROR,"decodec: failed");
+                    // 解码出错，处理错误
+                    break;
+                }else
+                {
+                    got_picture = 1;
+                }
+                if ( got_picture > 0 )
+                {
+                    swr_convert(au_convert_ctx,&out_buffer, MAX_AUDIO_FRAME_SIZE,(const uint8_t **)pFrame->data , pFrame->nb_samples);
+    #if 1
+                    printf("index:%5d\t pts:%lld\t packet size:%d\n",index,packet->pts,packet->size);
+    #endif
 
-        while(audio_len>0)//Wait until finish
-            SDL_Delay(1);
+
+    #if OUTPUT_PCM
+                    //Write PCM
+                    fwrite(out_buffer, 1, out_buffer_size, pFile);
+    #endif
+                    index++;
+                }
+            }
+
+#if USE_SDL
+            while(audio_len>0)//Wait until finish
+                SDL_Delay(1);
+
+            //Set audio buffer (PCM data)
+            audio_chunk = (Uint8 *) out_buffer;
+            //Audio buffer length
+            audio_len =out_buffer_size;
+            audio_pos = audio_chunk;
+
+#endif
+        }
+        av_packet_unref(packet);
     }
-    free(pcm_buffer);
+
+    swr_free(&au_convert_ctx);
+
+#if USE_SDL
+    SDL_CloseAudio();//Close SDL
     SDL_Quit();
+#endif
+
+#if OUTPUT_PCM
+    fclose(pFile);
+#endif
+    av_free(out_buffer);
+    avcodec_close(pCodecCtx);
+    avformat_close_input(&pFormatCtx);
+
     return 0;
 }
+
+
